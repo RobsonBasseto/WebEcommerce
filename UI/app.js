4bb5a56982057467538b28f8a435546ba9f39683
Robson Rafael de Oliveira Basseto <72422453+RobsonRafaeldeOliveiraBasseto@users.noreply.github.com>
2022-08-02 17:29:18 -0300

    alteração na branch ui

---
+++ UI/app.js
@@ -0,0 +1,18 @@
+const routes=[
+    {path:'/',component:home},
+    {path:'/usuario',component:usuario},
+    {path:'/categoria',component:categoria},
+    {path:'/lanche',component:lanche},
+    {path:'/pedido',component:pedido}
+]
+
+const router = new VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+
+const app = Vue.createApp({})
+
+app.use(router)
+
+app.mount('#app')
