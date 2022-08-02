588732222eb97f39fa25228d8df48a4956b4736d
Robson Rafael de Oliveira Basseto <72422453+RobsonRafaeldeOliveiraBasseto@users.noreply.github.com>
2022-08-02 18:25:27 -0300

    Inicio do desenvolvimento da tela de login

--- UI/usuario.js
+++ UI/usuario.js
@@ -1,1 +1,38 @@
-const usuario={template:'<body><h3 class="d-flex justify-content-center">Tela Home</h3>'}
+const usuario={template:`
+<div id="container-login" class ="container"> <!--Tela de Login-->
+                    <div class="form-group">
+                            <div class="row">
+                                    <div class="col-md-5"style="text-align: center">
+                                        <label >Usuário:</label>
+                                        <input type="text" class="form-control" id="ovTXT_usuario"/>
+                                    </div>
+                                </div>
+                    </div>
+                    <div class="form-group">
+                            <div class="row">
+                                    <div class="col-md-5" style="text-align: center">
+                                            <label >Senha:</label>
+                                            <input type="password" class="form-control" id="ovTXT_senha"/>
+                                        </div>
+                                </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-5" style="text-align: center">
+                                <button type="button" class="btn btn-primary" id="ovBTN_login" >Entrar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+</div>
+                    
+`,
+data(){
+
+},
+methods:{
+
+}
+}
