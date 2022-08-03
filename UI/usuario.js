e3016638269f0779c13fe11a8d07648dbe465e28
Robson Rafael de Oliveira Basseto <robsonrafaelfacul@gmail.com>
2022-08-02 21:36:39 -0300

    Conclusão da tela de login

--- UI/usuario.js
+++ UI/usuario.js
@@ -1,32 +1,30 @@
 const usuario={template:`
-<div id="container-login" class ="container"> <!--Tela de Login-->
-                    <div class="form-group">
-                            <div class="row">
-                                    <div class="col-md-5"style="text-align: center">
-                                        <label >Usuário:</label>
-                                        <input type="text" class="form-control" id="ovTXT_usuario"/>
-                                    </div>
-                                </div>
-                    </div>
-                    <div class="form-group">
-                            <div class="row">
-                                    <div class="col-md-5" style="text-align: center">
-                                            <label >Senha:</label>
-                                            <input type="password" class="form-control" id="ovTXT_senha"/>
-                                        </div>
-                                </div>
-                    </div>
-                    
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-md-5" style="text-align: center">
-                                <button type="button" class="btn btn-primary" id="ovBTN_login" >Entrar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    
-</div>
+<div id="container-login">
+        <h3>Login</h3>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-md-12">
+                    <label >E-mail:</label>
+                    <input type="text" class="form-control"/>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-md-12">
+                    <label >Senha:</label>
+                    <input type="password" class="form-control"/>
+                </div>
+            </div>
+        </div>
+        <a href="#">Esqueceu a senha?</a>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-md-6">
+                    <button type="button" class="btn btn-primary" id="ovBTN_login" ><i class="fa fa-sign-in-alt"></i> Entrar</button>
+                </div>
+            </div>
+        <label>Não possui conta? <a href="#">Crie sua conta</a></label>
                     
 `,
 data(){
